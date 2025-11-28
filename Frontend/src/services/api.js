102092e904ab05a8b30bea1356935c26ab06ab51
NssGourav <gourav341111@gmail.com>
2025-11-29 03:33:20 +0530

    chore : Fixed Timeout exceeded

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,28 @@
 import axios from "axios";
+import Constants from 'expo-constants';
+import * as SecureStore from "expo-secure-store";
+
+const ApiURL = () => {
+  const debuggerHost = Constants.expoConfig?.hostUri;
+  if (debuggerHost) {
+    const host = debuggerHost.split(':')[0];
+    return `http://${host}:3000`;
+  }
+  return "http://localhost:3000";
+};
 
 const API = axios.create({
-  baseURL: "http://10.254.202.225:3000",   //your LAN IP
+  baseURL: ApiURL(),
+  headers: { 'Content-Type': "application/json" }
+});
+
+// Interceptor to attach token to all requests
+API.interceptors.request.use(async (config) => {
+  const token = await SecureStore.getItemAsync("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default API;
